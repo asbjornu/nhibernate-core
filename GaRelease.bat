ff5b6f3cca122204eb85a94063fa77bd776c4365
asbjornu <asbjorn@ulsberg.no>
2011-09-09 13:00:44 +0200

    Added t=net-3.5 to the NAnt build command line and some error handling to GaRelease.bat

--- GaRelease.bat
+++ GaRelease.bat
@@ -1,1 +1,18 @@
-NAnt -D:project.config=release clean package
+@echo off
+NAnt.exe -t:net-3.5 -D:project.config=release clean package
+
+if %ERRORLEVEL%==0 (
+	echo.
+	echo Done!
+	echo.
+) else (
+	echo.
+	echo BUILD ERROR!
+	echo.
+	echo NAnt build error or NAnt not found. If you don't have NAnt,
+	echo please install it from http://nant.sourceforge.net/
+	echo and add it to your command line PATH.
+	echo.
+	echo.
+	pause
+)
